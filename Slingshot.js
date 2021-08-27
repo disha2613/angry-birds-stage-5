868a46dfb460895e183f3997e7d7b6095e6ccd46
disha2613 <82595224+disha2613@users.noreply.github.com>
2021-08-27 16:01:14 +0530

    Add files via upload

--- Slingshot.js
+++ Slingshot.js
@@ -3,7 +3,7 @@ class SlingShot{
         var options = {
             bodyA: bodyA,
             pointB: pointB,
-            stiffness: 0.04,
+            stiffness: 0.08,
             length: 10
         }
         this.sling1 = loadImage('sprites/sling1.png');
@@ -13,10 +13,14 @@ class SlingShot{
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
+    attach(body){
+        this.sling.bodyA = body;
+    }
 
     fly(){
         this.sling.bodyA = null;
     }
+    
 
     display(){
         image(this.sling1,200,20);
